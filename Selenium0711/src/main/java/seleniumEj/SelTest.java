9ea2127c8b82b9f2e4d2690bd23bf54488f7c9ac
Marcial Rivera <marcial.rivera@autozone.com>
2020-11-14 09:36:56 -0600

    cambio en SelTest.java

--- Selenium0711/src/main/java/seleniumEj/SelTest.java
+++ Selenium0711/src/main/java/seleniumEj/SelTest.java
@@ -11,10 +11,10 @@ public class SelTest {
 	public static void main(String[] args) {
 		System.out.println("prueba");
 		
-//		String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "drivers" + File.separator; //Seteand el Path de nuestro driver
-//		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe"); //Identificando el .exe para driver
-//		WebDriver driver = new ChromeDriver();//Creacion en tiempo de ejecucion de nuestro ChromeDriver
-//		driver.get("https://www.cooltesters.com/tienda-de-cursos/selenium-con-java");//Abriendo con la URL especificada
+		String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "drivers" + File.separator; //Seteand el Path de nuestro driver
+		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe"); //Identificando el .exe para driver
+		WebDriver driver = new ChromeDriver();//Creacion en tiempo de ejecucion de nuestro ChromeDriver
+		driver.get("https://www.cooltesters.com/tienda-de-cursos/selenium-con-java");//Abriendo con la URL especificada
 
 	}
 
