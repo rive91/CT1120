2244cfe78889f7eda08b8049f4e26a6971a75bab
Marcial Rivera <marcial.rivera@autozone.com>
2020-11-14 08:49:38 -0600

    First Commit

---
+++ Selenium0711/src/main/java/seleniumEj/SelTest.java
@@ -0,0 +1,20 @@
+package seleniumEj;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelTest {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+//		String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "drivers" + File.separator; //Seteand el Path de nuestro driver
+//		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe"); //Identificando el .exe para driver
+//		WebDriver driver = new ChromeDriver();//Creacion en tiempo de ejecucion de nuestro ChromeDriver
+//		driver.get("https://www.cooltesters.com/tienda-de-cursos/selenium-con-java");//Abriendo con la URL especificada
+
+	}
+
+}
---
+++ Selenium0711/src/test/java/Test/TestNGEj.java
@@ -0,0 +1,12 @@
+package Test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNGEj {
+  @Test
+  public void f() {
+	  Assert.assertTrue(true);
+	  
+  }
+}
